9e43fe853435cab9d47996ae4bcbb9b0db39b8e1
The Great J <jeffrey410469@gmail.com>
2015-08-25 12:28:12 +0800

    create commiittee model

---
+++ test/models/committee_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: committees
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text(65535)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'test_helper'
+
+class CommitteeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
