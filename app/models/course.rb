0a4fb932f26c120dc0a98aa96bef2ea95736ba84
wayne5540 <w5540@yahoo.com.tw>
2015-11-08 18:30:53 +0800

    annotate

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :integer          not null, primary key
+#  evaluation_id :integer
+#  name          :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Course < ActiveRecord::Base
   belongs_to :evaluation
 
