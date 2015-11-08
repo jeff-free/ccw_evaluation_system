ff0ea2b9bb89fc40ab02f7997f1264316c884edc
RJ <jeffrey410469@gmail.com>
2015-11-08 14:45:47 +0800

    Hotfix for params[:current_evaluation_id]

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,6 +1,6 @@
 module Admin::BaseHelper
   def current_evaluation_full_name
-    if params[:evaluation].present?
+    if params[:current_evaluation_id].present?
       "目前會期:#{@evaluation.full_name}"
     else
       "所有會期"
