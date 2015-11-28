02705705373c75b77c0a7dcb7f7b4ee72f6a41af
RJ <jeffrey410469@gmail.com>
2015-11-28 22:47:52 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -51,10 +51,11 @@ ActiveRecord::Schema.define(version: 20151128120055) do
   end
 
   create_table "committees", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "name",           limit: 255
+    t.integer  "committee_type", limit: 4,     default: 1
+    t.text     "description",    limit: 65535
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
   end
 
   create_table "congressmen", force: :cascade do |t|
