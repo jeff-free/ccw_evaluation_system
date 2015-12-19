30c8319dc9f42022bde8440a605da0d977a49650
RJ <jeffrey410469@gmail.com>
2015-12-19 16:32:48 +0800

    add several questions to survey and fixed volunteer survey record issue

--- app/controllers/volunteer/surveys_controller.rb
+++ app/controllers/volunteer/surveys_controller.rb
@@ -1,5 +1,8 @@
 class Volunteer::SurveysController < Volunteer::BaseController
 
+  before_action :set_inquiry!
+  before_action :set_evaluation!
+
   def new
     @survey = current_user.surveys.build
     @questions = @evaluation.questions.all
@@ -8,9 +11,7 @@ class Volunteer::SurveysController < Volunteer::BaseController
 
   def create
     @survey = current_user.surveys.build(survey_params)
-    @survey.evaluation = @evaluation
-    @survey.inquiry = @inquiry
-    @survey.user_role = current_user.role
+    @survey.assign_attributes(evaluation: @evaluation, inquiry: @inquiry, user_role: current_user.role)
 
     if @survey.save
       redirect_to root_path, notice: '問卷已經順利提交。'
@@ -21,13 +22,23 @@ class Volunteer::SurveysController < Volunteer::BaseController
 
   private
 
-    def set_inquiry
-      @inquiry = Inquiry.find(params[:id])
+    def set_evaluation!
+      @evaluation = @inquiry.evaluation
     end
 
-    
+    def set_inquiry!
+      @inquiry = Inquiry.find(params[:inquiry_id])
+    end
 
     def survey_params
-      params[:survey].permit(answers_attributes: [:question_id, :point])
+      params[:survey].permit(
+        :impression_before_and_after_watching_inquiry,
+        :comment_to_the_congressman,
+        :whether_to_recommend_the_inquiry,
+        :whether_inclusion_of_unappropriate_conversion,
+        :suggestion_of_survey,
+        :inquiry_id,
+        answers_attributes: [:question_id, :point]
+      )
     end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,13 +2,18 @@
 #
 # Table name: surveys
 #
-#  id            :integer          not null, primary key
-#  user_id       :integer
-#  evaluation_id :integer
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  inquiry_id    :integer
-#  user_role     :string(255)
+#  id                                            :integer          not null, primary key
+#  user_id                                       :integer
+#  evaluation_id                                 :integer
+#  created_at                                    :datetime         not null
+#  updated_at                                    :datetime         not null
+#  inquiry_id                                    :integer
+#  user_role                                     :string(255)
+#  impression_before_and_after_watching_inquiry  :text(65535)
+#  comment_to_the_congressman                    :text(65535)
+#  whether_to_recommend_the_inquiry              :text(65535)
+#  whether_inclusion_of_unappropriate_conversion :text(65535)
+#  suggestion_of_survey                          :text(65535)
 #
 
 class Survey < ActiveRecord::Base
