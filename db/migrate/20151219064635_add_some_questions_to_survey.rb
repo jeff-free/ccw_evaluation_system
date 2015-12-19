1f5599840b4b7b477a00176800495543c2a7d9ad
Rola Jeffrey <jeffrey410469@gmail.com>
2015-12-19 16:36:50 +0800

    Revert "add several questions to survey and fixed volunteer survey record issue"

--- db/migrate/20151219064635_add_some_questions_to_survey.rb
+++
@@ -1,9 +0,0 @@
-class AddSomeQuestionsToSurvey < ActiveRecord::Migration
-  def change
-    add_column :surveys, :impression_before_and_after_watching_inquiry, :text
-    add_column :surveys, :comment_to_the_congressman, :text
-    add_column :surveys, :whether_to_recommend_the_inquiry, :text
-    add_column :surveys, :whether_inclusion_of_unappropriate_conversion, :text
-    add_column :surveys, :suggestion_of_survey, :text
-  end
-end
